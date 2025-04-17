ee9f14c8b07b5f5bed3ccf17d94d7a002341d93c
Hritvik Soni <hritviksoni768@gmail.com>
2025-04-17 12:16:16 +0530

    alloed all header in filter

--- src/main/java/com/abelini_routing_middleware_poc/config/Slf4jMDCFilter.java
+++ src/main/java/com/abelini_routing_middleware_poc/config/Slf4jMDCFilter.java
@@ -22,7 +22,7 @@ public class Slf4jMDCFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         filterChain.doFilter(request, response);
 	}
